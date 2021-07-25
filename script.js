4e21607c43d7a969448585d23ac4be1b25e09675
jeraldnapkin <krpan.danijel1@gmail.com>
2021-07-25 21:36:26 +0200

    added UI for RPS game

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 
 // Generating random choice
 
-
+const userName = prompt('Hello, enter your name:', '')
 
 
 let test = '';
@@ -40,17 +40,55 @@ let score = 0;
 let round = 0;
 
 
+let playerSelection =''
 
 
+const btnRock = document.querySelector('#btn-1')
+btnRock.addEventListener('click', function ()  {
+    
+    playerSelection = 'rock';
+    game();
+    
+});
 
+const btnPaper = document.querySelector('#btn-2')
+btnPaper.addEventListener('click', function () {
+    
+    playerSelection = 'paper';
+    game();
+    
+});
+
+
+const btnScissors = document.querySelector('#btn-3')
+btnScissors.addEventListener('click', function () {
+    
+    playerSelection = 'scissors';
+    game();
+    
+});
+
+console.log(playerSelection);
 
 // Recalling functions and generating user input
 
+
+
+const computerSelected = document.querySelector('.computerSel')
+const playerSelected = document.querySelector('.playerSel')
+const outcomeInfo  = document.querySelector('.info')
+const currentRound = document.querySelector('.round')
+const playerScoretest = document.querySelector('.playerScore')
+const computerScore = document.querySelector('.computerScore')
+
 function game() {
+    
     console.clear();
     computer = computerPlay();
-    let playerSelection = prompt('Enter Rock,paper or scissors: ', '')
-    playerSelection = playerSelection.toLowerCase();
+    computerSelected.textContent = `Computer selected:      ${computer}`;
+    playerSelected.textContent = `${userName} selected: ${playerSelection}`;
+      // let playerSelection = prompt('Enter Rock,paper or scissors: ', '')
+    //playerSelection = playerSelection.toLowerCase();
     console.log(`Computer selected: ${computer}`);
     console.log(`Player selected: ${playerSelection}`)
     win = winner(playerSelection,computer);
@@ -58,13 +96,19 @@ function game() {
     if (win === 'You win!') {
         user_score++;
         round++;
+        outcomeInfo.textContent = 'You win!'
     } else if (win === 'You lose!') {
         pc_score++;
         round++;
+        outcomeInfo.textContent = 'You lose!'
     } else {
         round++;
+        outcomeInfo.textContent = 'It\'s a tie!'
     }
-
+    
+    currentRound.textContent =`Current round: ${round}`
+    computerScore.textContent=`Computer score: ${pc_score}`
+    playerScoretest.textContent=`Player score: ${user_score}`
     
 
     console.log(win)
@@ -74,16 +118,21 @@ function game() {
     
     if (user_score === 5) {
         console.log('You win first to 5!')
+        alert('You win first to 5!')
         user_score = 0;
         pc_score = 0;
         round = 0;
         console.log("##### Reseting results ######")
+        outcomeInfo.textContent = 'You won the game!'
+
     } else if (pc_score === 5) {
         console.log('Computer wins first to 5!')
+        alert('Computer wins first to 5!')
         user_score = 0;
         pc_score = 0;
         round = 0;
         console.log("##### Reseting results ##### ")
+        outcomeInfo.textContent = ' You lost the game!'
     } else {
         console.log('Game goes on...')
     }
@@ -91,6 +140,7 @@ function game() {
 }
 
 
-game();
+
+
 
 
